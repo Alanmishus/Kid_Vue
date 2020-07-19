d3fbb9ad46331cc2a7d7e3b5fe1cc85b1af9e195
Alanmishus <alanmishus0902@gmail.com>
2020-07-19 12:23:22 +0800

    vue tag prop bind

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-        msg: 'Hello Vue!'
+        msg: 'Hello Vue! 123',
+        align: 'v bind align',
+        center: 'center',
+        google: 'https://www.google.com/',
+        blank: '_blank'
     }
 });
