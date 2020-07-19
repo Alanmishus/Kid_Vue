deba8baeae9dfd618c609987863bdbf24cf5108b
Alanmishus <alanmishus0902@gmail.com>
2020-07-19 16:53:59 +0800

    subpage switch

--- main.js
+++ main.js
@@ -6,6 +6,37 @@ var app = new Vue({
         center: 'center',
         google: 'https://www.google.com/',
         blank: '_blank',
-        boolean: false
+        boolean: true,
+        
+    }
+});
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        products:[
+            {name:'ASUS',price:30000},
+            {name:'ACER',price:25000},
+            {name:'IPhone',price:22000},
+            {name:'Samsung',price:20000},
+            {name:'Sony',price:19000}
+        ]
+    }
+});
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        page: 0,
+        pageConfig: [
+            {btn:'page1',msg:'This is Page one.',code: 1},
+            {btn:'page2',msg:'This is Page two.',code: 2},
+            {btn:'page3',msg:'This is Page three.',code: 3}
+        ]
+    },
+    methods:{
+        switchPage(code){
+            this.page = code;
+        }
     }
 });
